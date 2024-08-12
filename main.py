c0dea8ddacbb2a5e6d93824429edc5033db02186
Maria <masya.2506@mail.ru>
2024-08-12 12:12:20 +0300

    com3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-app = ApplicationBuilder().token("TOKEN").build()
+app = ApplicationBuilder().token("7057785091:AAFY746rZSq9lxGP69Bw_6mbU1JbJgCxRm4").build()
 
 app.add_handler(CommandHandler("hi", hi_command))
 app.add_handler(CommandHandler("help", help_command))
